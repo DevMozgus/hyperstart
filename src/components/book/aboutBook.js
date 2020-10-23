c8d1bdc972320017a116c5c8dd45581a5c5f7b8c
Nicola Urlicic <nurlicic@gmail.com>
2020-10-23 13:43:19 +0200

    spelling error and title change

--- src/components/book/aboutBook.js
+++ src/components/book/aboutBook.js
@@ -6,13 +6,13 @@ export const aboutBook = (props) => {
   return html`
   <section class="bookDescription">
     <div>
-    <h1>Now That's A Book I Would Read</h1>
+    <h1>Is That An O'Reilly Book Cover Generator?</h1>
     <p>That's right, after extensively researching hyperapp and the hyperstart's single blog post (RIP Blog), I decided to steal your entire work.</p>
     <p><b>I created this entire site using hyperapp.</b></p>
     <p>However, that wasn't enough. Within this stroke of genius, I also found hyperapp examples you hadn't implemented yet. Naturally, I had to steal that too.</p>
     <p>And so, I present you the <b>O'Reilly book cover generator™</b></p>
     </div>
-    <a href="https://github.com/DevMozgus/hyperstart">View This Sites Code</a>
+    <a href="https://github.com/DevMozgus/hyperstart">View This Site's Code</a>
     </section>
   `
 }
